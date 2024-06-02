9f2cc6ca4563a97b00266d58bef2f737f3a2931d
vi_vio <77623828+vivio2115@users.noreply.github.com>
2024-06-03 00:49:08 +0200

    Update connection.php

--- core/database/connection.php
+++ core/database/connection.php
@@ -1,12 +1,18 @@
 <?php 
-	$dsn = 'mysql:host=172.93.105.132; dbname=db_los_angos_dc';
-	$user = 'root';
-	$password = 'dIpG01c_*ssaB0ai0A+EckjzzMGf@b9A75C1E@#Az';
- 
+$dsn = 'mysql:host=172.93.105.132;dbname=db_los_angos_dc';
+$user = 'root';
+$password = 'dIpG01c_*ssaB0ai0A+EckjzzMGf@b9A75C1E@#Az';
 
-	try{
-		$pdo = new PDO($dsn, $user, $password);
-	}catch(PDOException $e){
-		echo 'connection error! ' . $e->getMessage();
-	}	
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+} catch (PDOException $e) {
+    echo 'Connection error! ' . $e->getMessage();
+    exit();
+}
 ?>
